48bd3c1cf90fd80426b1d2be786ae93c9e5ba626
aarthi-9 <aarthi.edara@lntinfotech.com>
2021-09-15 18:06:51 +0530

    commit

---
+++ JunitProject/src/com/lti/java/Bank.java
@@ -0,0 +1,12 @@
+package com.lti.java;
+
+public class Bank {
+
+	public static SavingsAccount getObject() {
+		// TODO Auto-generated method stub
+		//return null;
+		return new SavingsAccount(101,"julie",5000);
+
+	}
+
+}
